798fa00729ff10acbcee1a7ec37087574c5c66ff
krishpranav <krisna.pranav@gmail.com>
2023-03-05 18:58:08 +0530

    types: inex, hooks: useInterval, useIsomorphicLayoutEffects

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,33 +1,40 @@
-export type AppType = 'search' | 'content' | 'favourites' | 'settings'
+export type AppType = 'search' | 'content' | 'favorites' | 'settings'
 
 export interface App {
-    id: string 
-
-    subtitle: string 
-
-    prompt: string 
-
+  id: string
+  title: string
+  subtitle: string
+  prompt: string
+  tasks: Record<string, string>
+  html: string
+  text: Record<string, string>
+  script: string
+  data: Record<string, any>
 }
 
 export interface Tab {
-    isActive: boolean
-    isFavorite: boolean
-    type: AppType
-    isNew: boolean
+  isActive: boolean
+  isFavorite: boolean
+  type: AppType
+  isNew: boolean
 }
 
 export type AppTab = App & Tab
 
 export interface Link {
-    title: string
-    alt: string
+  title: string
+  alt: string
 }
 
 export interface Settings {
-    coreVendor: string
-    huggingFaceKey: string
-    openAIKey: string
-    openAIModel: string
-    customTasksPrompt: string
-    useMockData: boolean
+  coreVendor: string
+  huggingFaceKey: string
+  huggingFaceModel: string
+  openAIKey: string
+  openAIModel: string
+  customTasksPrompt: string
+  customHtmlPrompt: string
+  customScriptPrompt: string
+  useAutoCherryPick: boolean
+  useMockData: boolean
 }
