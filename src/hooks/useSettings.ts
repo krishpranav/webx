6604aae804a7a7c4aa47f67883b2acfd545d999d
krishpranav <krisna.pranav@gmail.com>
2023-03-05 19:14:55 +0530

    hooks: usePreventPageRefresh, useSettings

---
+++ src/hooks/useSettings.ts
@@ -0,0 +1,21 @@
+import { useLocalStorage } from '@mantine/hooks'
+import { Settings } from '../types'
+
+export const useSettings = (
+  defaultValue: Settings = {
+    coreVendor: 'OpenAI',
+    huggingFaceKey: '',
+    huggingFaceModel: 'EleutherAI/gpt-j-6B',
+    openAIKey: '',
+    openAIModel: 'text-davinci-003',
+    customTasksPrompt: '',
+    customHtmlPrompt: '',
+    customScriptPrompt: '',
+    useMockData: false,
+    useAutoCherryPick: false,
+  }
+) =>
+  useLocalStorage<Settings>({
+    key: 'settings-rev-2',
+    defaultValue,
+})
