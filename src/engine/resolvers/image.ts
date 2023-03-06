7c632556d0fb2f684961d882c57879066cb6690c
krishpranav <krisna.pranav@gmail.com>
2023-03-06 19:48:35 +0530

    engine: resolvers[image spinning functionalities]

--- src/engine/resolvers/image.ts
+++ src/engine/resolvers/image.ts
@@ -1,28 +1,44 @@
 import spinner from '../../assets/spinner.gif'
 import { DalleImage } from '../../providers/openai/types'
 
-export async function resolveImage(
-    model: string,
-    apiKey: string,
-    useMockData: boolean
+export async function resolveImages(
+  model: string,
+  apiKey: string,
+  useMockData: boolean
 ) {
-    if (useMockData) { 
-        return ''
-    }
+  if (useMockData) {
+    return ''
+  }
+  const images = document.getElementsByTagName('img')
 
-    const images = document.getElementsByTagName('img')
+  for (let i = 0; i < images.length; i++) {
+    const src = images[i].src
+    const alt = images[i].alt
+    console.log('resolveImages> found image:', { alt, src })
+    if (alt) {
+      console.log('resolveImages> found image:', alt)
+      try {
+        images[i].src = spinner.src
+        const response = await fetch(
+          `/api/image?prompt=${encodeURIComponent(
+            alt
+          )}&model=${encodeURIComponent(model)}&apiKey=${encodeURIComponent(
+            apiKey
+          )}`
+        )
+        const { url, prompt, width, height } =
+          (await response.json()) as DalleImage
 
-    for (let i = 0; i < images.length; i++) {
-        const src = images[i].src
-        const alt = images[i].alt
-        console.log('resolveImages> found image: ', {alt , src})
-        if (alt) {
-            console.log('resolveImages> found image:', alt)
-            try {
-                images[i].src = spinner.src
-            } catch (err) {
-                console.log('resolveImages> failed to replace an image, weired', err)
-            }
-        }
+        console.log(
+          'resolveImages> replacing src with url and deleting alt',
+          url
+        )
+        images[i].src = url
+        images[i].removeAttribute('alt')
+      } catch (err) {
+        console.log('resolveImages> failed to replace an image, weird', err)
+      }
     }
+  }
+
 }
