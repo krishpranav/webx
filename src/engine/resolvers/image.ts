3e6fe25d0c36647d39046386bb7d9fee62187954
krishpranav <krisna.pranav@gmail.com>
2023-03-06 19:48:01 +0530

    engine: resolvers[image functionalities]

--- src/engine/resolvers/image.ts
+++ src/engine/resolvers/image.ts
@@ -1,1 +1,28 @@
-// 
+import spinner from '../../assets/spinner.gif'
+import { DalleImage } from '../../providers/openai/types'
+
+export async function resolveImage(
+    model: string,
+    apiKey: string,
+    useMockData: boolean
+) {
+    if (useMockData) { 
+        return ''
+    }
+
+    const images = document.getElementsByTagName('img')
+
+    for (let i = 0; i < images.length; i++) {
+        const src = images[i].src
+        const alt = images[i].alt
+        console.log('resolveImages> found image: ', {alt , src})
+        if (alt) {
+            console.log('resolveImages> found image:', alt)
+            try {
+                images[i].src = spinner.src
+            } catch (err) {
+                console.log('resolveImages> failed to replace an image, weired', err)
+            }
+        }
+    }
+}
